ecdf4f5f929b991bcffec495f6df7204aaa6c8d8
tang <3083649887@qq.com>
2023-11-29 00:34:37 +0800

    add

---
+++ JQuery/discuss/js/index.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    let Rated = false;
+    let RatedLevel = 0;
+    $("#ShowImageBtn").on("click", function () {
+        $("#myImage").slideToggle();
+    });
+    $(".star").on("mouseenter", function () {
+        let index = $(this).index();
+
+        for (var i = 0; i <= 4; i++) {
+            var DomID = "#star" + (i);
+            $(DomID).removeClass("star-Active");
+        }
+        for (var i = 0; i <= index; i++) {
+            var DomID = "#star" + (i);
+            $(DomID).addClass("star-Active");
+        }
+    })
+    $(".star").on("click", function () {
+        Rated = true;
+        RatedLevel = $(this).index() + 1;
+        console.log(RatedLevel);
+        return RatedLevel;
+
+    })
+    $(".Rating_Box").on("mouseleave", function () {
+        if (Rated == false) {
+            for (var i = 0; i <= 4; i++) {
+                var DomID = "#star" + (i);
+                $(DomID).removeClass("star-Active");
+            }
+        } else {
+            Rated = false;
+        }
+    })
+
+    $("#Submit_Btn").on("click", function () {
+        let MsgText = $("#Text_Input").val();
+
+        if (MsgText) {
+            let StarDom = (RatedLevel === 0) ? "未评级 " : "&#9734;".repeat(RatedLevel) + " ";
+            let SubmitDom =
+                `<div>   <span class="RatedStars"> ` + StarDom + `</span><span>` + MsgText + `</span></div>`
+            if ( $(".NoDis").length <= 0) {
+                $("#Msg_Box").append(SubmitDom);
+            } else {
+                $("#Msg_Box").html(SubmitDom);
+            }
+        } else {
+            alert("未输入评论！")
+        }
+        RatedLevel = 0;
+        Rated = false;
+        $(".star").removeClass("star-Active");
+        $("#Text_Input").val("");
+    })
+});
