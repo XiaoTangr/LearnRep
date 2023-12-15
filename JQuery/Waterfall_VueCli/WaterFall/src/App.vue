d08718376b085b397f4f1f096762f0f823b84aa0
tang <3083649887@qq.com>
2023-12-15 14:59:07 +0800

    add

---
+++ JQuery/Waterfall_VueCli/WaterFall/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <RouterTemplate></RouterTemplate>
+    <RouterView></RouterView>
+  </div>
+</template>
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import RouterTemplate from "@/components/Public/Router.vue";
+</script>
+<style scoped>
+@media (min-width: 1024px) {
+}
+</style>
