d4ee0bf9367529a2bf94e2dc86e875a7e1e6e580
tang <3083649887@qq.com>
2023-12-26 19:30:09 +0800

    upda

--- JQuery/WarterFall/js/index.js
+++ JQuery/WarterFall/js/index.js
@@ -1,33 +1,44 @@
-
 class Waterfall {
     el = null;
-    Width = 0;
+    All_Width = 0;
+    Each_Width = 0;
     col_Num = 0;
     ImgArr = [];
-    col_Height = {};
     img_length = 0;
     loaded_index = 0;
+    First_load = 0;
     lock = false;
 
-    constructor(el, col_Num, Url) {
+    constructor(el, col_Num, First_load, Url) {
         this.el = el;
-        this.col_Num = col_Num;
+        this.All_Width = $(el).width();
+        if (this.All_Width <= 640) {
+            this.col_Num = 2;
+        } else if (this.All_Width > 640 && this.All_Width <= 960) {
+            this.col_Num = 3;
+        } else if (this.All_Width > 960 && this.All_Width <= 1280) {
+            this.col_Num = 4;
+        } else {
+            this.col_Num = col_Num;
+        }
+        this.Each_Width = this.All_Width / this.col_Num;
         this.url = Url;
+        this.First_load = First_load;
         this.LoadData()
         this.CreateCOL()
 
     }
     GetShortestCOL() {
-        let min = this.col_Height[0].height;
-        let min_index = 0;
+        var min = $("#WF_Col_0").height();
+        var min_index = 0;
         for (let i = 0; i < this.col_Num; i++) {
-            if (this.col_Height[i].height < min) {
-                min = this.col_Height[i].height;
+            if ($("#WF_Col_" + i).height() < min) {
                 min_index = i;
+                min = $("#WF_Col_" + i).height();
             }
-
         }
-        return this.col_Height[min_index].el;
+        // console.log($("#WF_Col_" + min_index).height())
+        return ("#WF_Col_" + min_index)
     }
 
     LoadData() {
@@ -39,11 +50,11 @@ class Waterfall {
             success: function (res) {
                 self.ImgArr = res.ImgArr;
                 self.img_length = res.ImgArr.length;
-                self.InsertItem();
+                self.InsertItem(self.First_load);
                 return res
             },
             error: function (err) {
-                return -1
+                return err
             }
         })
     }
@@ -52,62 +63,55 @@ class Waterfall {
             let $WF_COL = `<div class="WF_Col" id="WF_Col_` + i + `"></div>`
             this.el.append($WF_COL)
         }
-        this.UpdateHight()
+        $(".WF_Col").css("width", this.Each_Width + "px")
     }
 
 
-    // let $WF_Item = `
-    //     <div class="WF_Item" id="WF_Item_`+ IntoID + ID + `">
-    //     <img class="WF_Item_Img" id="WF_Item_Img_`+ IntoID + ID + `" src="` + ImgSrc + `" alt="#">
-    //     <p class="WF_Item_P" id="WF_Item_P_`+ IntoID + ID + `">` + Desc + `</p>
-    //     </div> `
-
-
-    InsertItem() {
+    InsertItem(num) {
         if (this.lock === false) {
-            for (let i = 0; i < 3 * this.col_Num; i++) {
-
-                let index = this.loaded_index;
-                if (this.loaded_index >= this.img_length) {
-                    this.lock = true;
-                    break;
-                }
-                let imgsrc = this.ImgArr[index].src;
-                let imgdesc = this.ImgArr[index].desc;
-                let $WF_Item = `
-                    <div class="WF_Item" id="WF_Item_`+ index + `">
-                    <img class="WF_Item_Img" id="WF_Item_Img_`+ index + `" src="` + imgsrc + `" alt="#">
-                    <p class="WF_Item_P" id="WF_Item_P_`+ index + `">` + imgdesc + `</p>
+            var self = this;
+            for (let i = 0; i < num; i++) {
+                setTimeout(function () {
+                    let min_dom_id = self.GetShortestCOL()
+                    let index = self.loaded_index;
+                    let imgsrc = self.ImgArr[index].src;
+                    let imgdesc = self.ImgArr[index].desc;
+                    let $WF_Item = `
+                    <div class="WF_Item" id="WF_Item_` + index + `">
+                    <img class="WF_Item_Img" id="WF_Item_Img_` + index + `" alt="#">
+                    <p class="WF_Item_P" id="WF_Item_P_` + index + `">` + imgdesc + `</p>
                     </div> `
-                this.UpdateHight()
-                $(this.GetShortestCOL()).append($WF_Item)
-                this.loaded_index++;
+                    $(min_dom_id).append($WF_Item)
+                    if (self.loaded_index >= self.img_length - 1) {
+                        self.lock = true;
+                        alert("达到底部。");
+                    }
+                    $("#WF_Item_Img_" + index).attr("src", imgsrc)
+                    self.loaded_index++;
+                }, 300 * i)
             }
         } else {
             console.log("locked")
         }
     }
-
-    UpdateHight() {
-        for (let i = 0; i < this.col_Num; i++) {
-            let newdata = {
-                "height": $("#WF_Col_" + i).height(),
-                "el": "#WF_Col_" + i
-            }
-            this.col_Height[i] = newdata
-        }
+    ResetWidth(int) {
+        $(this.el).width(int)
+        this.All_Width = $(this.el).width();
+        this.Each_Width = this.All_Width / this.col_Num;
+        $(".WF_Col").css("width", this.Each_Width + "px")
     }
 }
-
 $(document).ready(function () {
-    var WF = new Waterfall($("#WarterFall"),2, "./api/data.json")
-
-
+    var WF = new Waterfall($("#WarterFall"), 4, 16, "./api/data.json")
+    var minAwayBtm = 50; // 距离页面底部的最小距离
     $(window).scroll(function () {
-        if ($(window).scrollTop() + $(window).height() >= $(document).height() * 0.45) {
-            if (WF.lock === false) {
-                WF.InsertItem()
-            }
+        var awayBtm = $(document).height() - $(window).scrollTop() - $(window).height();
+        if (awayBtm <= minAwayBtm) {
+            // isbool = false;
+            WF.InsertItem(3)
         }
     });
+    $(window).resize(function () {
+        WF.ResetWidth($(window).width())
+    })
 })
